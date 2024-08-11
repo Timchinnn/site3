a0e4229b3c63fb7ef545600505e83dc24fa2648a
Timchinnn <nevarui62@gmail.com>
2024-08-11 17:35:32 +0300

    initial

--- src/App.js
+++ src/App.js
@@ -1,23 +1,45 @@
-import logo from './logo.svg';
-import './App.css';
+import React, { useEffect, useState } from "react";
+import axios from "axios";
+import "./App.css"; // Для стилей, создайте этот файл
 
 function App() {
+  const [products, setProducts] = useState([]);
+
+  const fetchProducts = () => {
+    axios
+      .get("http://217.18.62.19:3000/api/products")
+      .then((response) => {
+        setProducts(response.data);
+      })
+      .catch((error) => {
+        console.error("Ошибка при получении товаров:", error);
+      });
+  };
+
+  useEffect(() => {
+    fetchProducts(); // Получаем товары при первом рендере
+
+    // Устанавливаем интервал для обновления данных
+    const intervalId = setInterval(() => {
+      fetchProducts(); // Обновляем данные каждые 5 секунд
+    }, 5000);
+
+    // Очистка интервала при размонтировании компонента
+    return () => clearInterval(intervalId);
+  }, []);
+
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <h1>Магазин товаров</h1>
+      <div className="products">
+        {products.map((product) => (
+          <div key={product.id} className="product">
+            <img src={product.photo_url} alt={product.name} />
+            <h2>{product.name}</h2>
+            <p>{product.description}</p>
+          </div>
+        ))}
+      </div>
     </div>
   );
 }
