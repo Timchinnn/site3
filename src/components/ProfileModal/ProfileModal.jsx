4a47bd1f77b323c6fca9a05cd65c4ee8090dd322
Timchinnn <nevarui62@gmail.com>
2024-08-15 16:36:11 +0300

    modified:   src/components/ProfileModal/ProfileModal.jsx

--- src/components/ProfileModal/ProfileModal.jsx
+++ src/components/ProfileModal/ProfileModal.jsx
@@ -8,26 +8,77 @@ const ProfileModal = ({
   onRegisterComplete,
   telegramUserId,
 }) => {
+  const [formData, setFormData] = useState({
+    name: "",
+    phone: "",
+    email: "",
+  });
+
+  const handleChange = (e) => {
+    const { name, value } = e.target;
+    setFormData((prevData) => ({
+      ...prevData,
+      [name]: value,
+    }));
+  };
+
+  const handleSubmit = (e) => {
+    e.preventDefault();
+    if (formData.name && formData.phone && formData.email) {
+      const newUser = {
+        user_id: telegramUserId,
+        ...formData,
+      };
+      onRegisterComplete(newUser);
+    } else {
+      alert("Пожалуйста, заполните все поля.");
+    }
+  };
+
   return (
     <div className="modal">
       {isRegisterFormOpen ? (
         <div className="profile-button">
-          <h2>Регистрация </h2>
-          {/* Здесь размещаем вашу форму регистрации */}
-          <button
-            // className="profile-button"
-            onClick={() => {
-              const newUser = {
-                user_id: telegramUserId,
-                name: "Имя",
-                phone: "Телефон",
-                email: "Email",
-              };
-              onRegisterComplete(newUser); // Сохраняем пользователя
-            }}
-          >
-            Зарегистрироваться
-          </button>
+          <h2>Регистрация</h2>
+          <form onSubmit={handleSubmit}>
+            <div>
+              <label>
+                Имя:
+                <input
+                  type="text"
+                  name="name"
+                  value={formData.name}
+                  onChange={handleChange}
+                  required
+                />
+              </label>
+            </div>
+            <div>
+              <label>
+                Телефон:
+                <input
+                  type="tel"
+                  name="phone"
+                  value={formData.phone}
+                  onChange={handleChange}
+                  required
+                />
+              </label>
+            </div>
+            <div>
+              <label>
+                Email:
+                <input
+                  type="email"
+                  name="email"
+                  value={formData.email}
+                  onChange={handleChange}
+                  required
+                />
+              </label>
+            </div>
+            <button type="submit">Зарегистрироваться</button>
+          </form>
         </div>
       ) : (
         <div className="profile-button">
