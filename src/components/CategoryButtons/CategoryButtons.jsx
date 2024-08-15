b7dcdfb90fa6fed6ce0ff7ef9229a00f89732686
Timchinnn <nevarui62@gmail.com>
2024-08-15 12:54:41 +0300

    modified:   src/components/CategoryButtons/CategoryButtons.css 	modified:   src/components/CategoryButtons/CategoryButtons.jsx

--- src/components/CategoryButtons/CategoryButtons.jsx
+++ src/components/CategoryButtons/CategoryButtons.jsx
@@ -1,11 +1,23 @@
-import React from "react";
-import "./CategoryButtons.css"; // Создайте файл для стилей, если нужно
+import React, { useState } from "react";
+import "./CategoryButtons.css";
 
 const CategoryButtons = ({ categories, onSelect }) => {
+  const [activeButton, setActiveButton] = useState(null);
+
+  const handleButtonClick = (categoryName) => {
+    // Если кнопка уже активная, сбрасываем ее
+    setActiveButton(activeButton === categoryName ? null : categoryName);
+    onSelect(categoryName); // Срабатывает функция выбора
+  };
+
   return (
     <div className="category-buttons">
       {categories.map((category, index) => (
-        <button key={index} onClick={() => onSelect(category.name)}>
+        <button
+          key={index}
+          onClick={() => handleButtonClick(category.name)}
+          className={activeButton === category.name ? "active" : ""}
+        >
           {category.name}
         </button>
       ))}
