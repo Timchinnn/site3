4303ac1c7887ded1edf62093fc69c9a2c1fb7495
Timchinnn <nevarui62@gmail.com>
2024-08-18 21:05:49 +0300

    modified:   src/components/Product/Product.css 	modified:   src/components/Product/Product.js

--- src/components/Product/Product.js
+++ src/components/Product/Product.js
@@ -1,13 +1,7 @@
-import React, { useState } from "react";
+import React from "react";
 import "./Product.css";
 
 const Product = ({ product, quantity, onAdd, onRemove, openModal }) => {
-  const [isInCart, setIsInCart] = useState(false);
-
-  const handleAddToCartClick = () => {
-    setIsInCart(true);
-  };
-
   return (
     <div
       className={`product-card ${quantity > 0 ? "highlight" : ""}`}
@@ -19,24 +13,18 @@ const Product = ({ product, quantity, onAdd, onRemove, openModal }) => {
       </div>
       <div className="product-title">{product.name}</div>
       <div className="product-price-add">
-        {!isInCart ? (
-          <button className="add-to-cart" onClick={handleAddToCartClick}>
-            В корзину
-          </button>
-        ) : (
-          <div className="cart-controls">
-            {quantity > 0 && (
-              <button
-                className="add-to-cart-min"
-                onClick={(e) => {
-                  e.stopPropagation();
-                  onRemove(product);
-                }}
-              >
-                -
-              </button>
-            )}
-            <span className="product-quantity">{quantity}</span>
+        {quantity > 0 ? (
+          <div className="price-controls">
+            <button
+              className="add-to-cart-min"
+              onClick={(e) => {
+                e.stopPropagation();
+                onRemove(product);
+              }}
+            >
+              -
+            </button>
+            <div className="product-quantity">{quantity}</div>
             <button
               className="add-to-cart"
               onClick={(e) => {
@@ -47,6 +35,16 @@ const Product = ({ product, quantity, onAdd, onRemove, openModal }) => {
               +
             </button>
           </div>
+        ) : (
+          <button
+            className="add-to-cart"
+            onClick={(e) => {
+              e.stopPropagation();
+              onAdd(product);
+            }}
+          >
+            В корзину
+          </button>
         )}
       </div>
     </div>
