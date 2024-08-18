75421fa3c8176e1ae2f62d4b841554172823db89
Timchinnn <nevarui62@gmail.com>
2024-08-18 19:29:21 +0300

    modified:   src/App.js

--- src/App.js
+++ src/App.js
@@ -125,9 +125,7 @@ function App() {
     <div className="App">
       <h1>Магазин товаров</h1>
       <div className="profile-button">
-        <img src="profile.png" alt="">
-          "
-        </img>
+        <img src="profile.png" alt=""></img>
         <button onClick={handleProfileButtonClick}>Профиль</button>{" "}
       </div>
 
