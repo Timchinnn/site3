42c1633c8f5244dcc9ef889d049179297b8ef206
Timchinnn <nevarui62@gmail.com>
2024-08-18 22:03:46 +0300

    modified:   src/components/CartModal/CartModal.css 	modified:   src/components/CartModal/CartModal.jsx

--- src/components/CartModal/CartModal.jsx
+++ src/components/CartModal/CartModal.jsx
@@ -2,34 +2,36 @@ import React, { useEffect } from "react";
 import "./CartModal.css"; // Путь к стилям для модального окна
 
 const CartModal = ({ items, total, onClose }) => {
-  useEffect(() => {
-    if (items.length === 0) return;
+  if (!items.length) return null; // Если корзина пуста, ничего не отображаем
 
+  useEffect(() => {
     // Отключение прокрутки при монтировании компонента
     document.body.classList.add("no-scroll");
     return () => {
       // Включение прокрутки при размонтировании компонента
       document.body.classList.remove("no-scroll");
     };
-  }, [items]);
-
-  if (items.length === 0) return null; // Если корзина пуста, ничего не отображаем
+  }, []);
 
   return (
     <div className="modal-overlay">
       <div className="modal">
-        <h2>Ваша корзина</h2>
-        {items.map((item) => (
-          <div key={item.id} className="cart-item">
-            {/* <img src={item.photo_url} alt={item.name} /> */}
-            <span>{item.name}</span>
-            <span>
-              {item.quantity} × {item.price} ₽
-            </span>
-          </div>
-        ))}
-        <h3>Итого: {total} ₽</h3>
-        <button onClick={onClose}>Закрыть</button>
+        <button className="modal-close-button" onClick={onClose}>
+          <span className="icon">×</span>
+        </button>
+        <div className="modal-inner-content">
+          <h2>Ваша корзина</h2>
+          {items.map((item) => (
+            <div key={item.id} className="cart-item">
+              <span>{item.name}</span>
+              <span>
+                {item.quantity} × {item.price} ₽
+              </span>
+            </div>
+          ))}
+          <h3>Итого: {total} ₽</h3>
+          <button onClick={onClose}>Закрыть</button>
+        </div>
       </div>
     </div>
   );
