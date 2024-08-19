f670d5f3f65c796250fe683a837a640468ecd541
Timchinnn <nevarui62@gmail.com>
2024-08-19 13:32:45 +0300

    modified:   src/components/CartModal/CartModal.jsx

--- src/components/CartModal/CartModal.jsx
+++ src/components/CartModal/CartModal.jsx
@@ -1,4 +1,4 @@
-import React, { useEffect, useState } from "react";
+import React, { useEffect } from "react";
 import "./CartModal.css"; // Импортируем стили
 
 const CartModal = ({ items, total, onClose }) => {
