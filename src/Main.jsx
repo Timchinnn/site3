150144815078359ef0c1018a5a5aa8eb3024adaa
Timchinnn <nevarui62@gmail.com>
2024-09-07 15:09:34 +0300

    new file:   src/Main.css 	modified:   src/Main.jsx

--- src/Main.jsx
+++ src/Main.jsx
@@ -1,10 +1,38 @@
+import React, { useState } from "react";
 import App from "./App";
-import React from "react";
+
 function Main() {
+  const [showApp, setShowApp] = useState(false);
+
+  const handleShopButtonClick = () => {
+    setShowApp(true);
+  };
+
+  if (showApp) {
+    return <App />; // Если флаг установлен, возвращаем компонент App
+  }
+
   return (
-    <div>
-      <App />
+    <div className="container">
+      <div className="header">
+        <a href="#" className="link">
+          ВХОД
+        </a>
+        <a href="#" className="link">
+          РЕГИСТРАЦИЯ
+        </a>
+      </div>
+      <div className="content">
+        <h1 className="title">BANSYS</h1>
+        <p className="subtitle">Bansys - Банкоматы - Терминалы</p>
+      </div>
+      <div className="footer">
+        <button className="shop-button" onClick={handleShopButtonClick}>
+          ЗА ПОКУПКАМИ
+        </button>
+      </div>
     </div>
   );
 }
+
 export default Main;
