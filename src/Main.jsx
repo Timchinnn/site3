2530da70bbe2dfbe97797bad8cc3d345f2893249
Timchinnn <nevarui62@gmail.com>
2024-09-07 15:12:41 +0300

    modified:   src/Main.jsx

--- src/Main.jsx
+++ src/Main.jsx
@@ -4,33 +4,65 @@ import App from "./App";
 function Main() {
   const [showApp, setShowApp] = useState(false);
 
-  const handleShopButtonClick = () => {
+  const handleButtonClick = () => {
     setShowApp(true);
   };
 
-  if (showApp) {
-    return <App />; // Если флаг установлен, возвращаем компонент App
-  }
-
   return (
-    <div className="container">
-      <div className="header">
-        <a href="#" className="link">
-          ВХОД
-        </a>
-        <a href="#" className="link">
-          РЕГИСТРАЦИЯ
-        </a>
-      </div>
-      <div className="content">
-        <h1 className="title">BANSYS</h1>
-        <p className="subtitle">Bansys - Банкоматы - Терминалы</p>
-      </div>
-      <div className="footer">
-        <button className="shop-button" onClick={handleShopButtonClick}>
-          ЗА ПОКУПКАМИ
-        </button>
-      </div>
+    <div
+      style={{
+        margin: 0,
+        fontFamily: "Arial, sans-serif",
+        backgroundColor: "#3d6659",
+        display: "flex",
+        justifyContent: "center",
+        alignItems: "center",
+        height: "100vh",
+      }}
+    >
+      {showApp ? (
+        <App />
+      ) : (
+        <div className="container">
+          <div className="header">
+            <button
+              className="link"
+              onClick={() => alert("Вход")}
+              style={{
+                background: "none",
+                border: "none",
+                color: "#e1b95b",
+                textDecoration: "underline",
+                cursor: "pointer",
+              }}
+            >
+              ВХОД
+            </button>
+            <button
+              className="link"
+              onClick={() => alert("Регистрация")}
+              style={{
+                background: "none",
+                border: "none",
+                color: "#e1b95b",
+                textDecoration: "underline",
+                cursor: "pointer",
+              }}
+            >
+              РЕГИСТРАЦИЯ
+            </button>
+          </div>
+          <div className="content">
+            <h1 className="title">BANSYS</h1>
+            <p className="subtitle">Bansys - Банкоматы - Терминалы</p>
+          </div>
+          <div className="footer">
+            <button className="shop-button" onClick={handleButtonClick}>
+              ЗА ПОКУПКАМИ
+            </button>
+          </div>
+        </div>
+      )}
     </div>
   );
 }
