ee21b4563b6a45d5b4f516f15d38d0e7f5fe4580
Timchinnn <95688018+Timchinnn@users.noreply.github.com>
2024-09-14 15:46:29 +0000

    modified:   src/components/ProfileModal/ProfileModal.css 	modified:   src/components/ProfileModal/ProfileModal.jsx

--- src/components/ProfileModal/ProfileModal.jsx
+++ src/components/ProfileModal/ProfileModal.jsx
@@ -1,13 +1,16 @@
 import React from "react";
 import "./ProfileModal.css";
 
-// import ".";
-// import "./i18n";
-
-const ProfileModal = () => {
+const ProfileModal = ({ onClose }) => {
   return (
-    <div className="modal">
-      <p>ertyty</p>
+    <div className="modal-overlay">
+      <div className="modal-content">
+        <h2>Личный кабинет</h2>
+        <p>Здесь вы можете управлять своим профилем.</p>
+        {/* Добавьте здесь дополнительные элементы, такие как форма или информация о пользователе */}
+        <button onClick={onClose}>Закрыть</button>{" "}
+        {/* Кнопка для закрытия модального окна */}
+      </div>
     </div>
   );
 };
