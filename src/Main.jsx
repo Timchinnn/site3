a35fcdafc16350c6f0baaafcff4826aed704eed8
Timchin-cpu <83727119+Timchin-cpu@users.noreply.github.com>
2024-09-17 15:18:59 +0000

    modified:   src/Main.jsx 	modified:   src/index.js

--- src/Main.jsx
+++ src/Main.jsx
@@ -1,103 +1,54 @@
-// import React, { useState } from "react";
-// import App from "./App";
-// import "./Main.css"; // Убедитесь, что стили подключены
-// import bansys from "./bansys.png";
-// import toBuy from "./tobuy.png";
-
-// function Main() {
-//   const [showApp, setShowApp] = useState(false);
-
-//   const handleShopButtonClick = () => {
-//     setShowApp(true);
-//   };
-
-//   const handleLoginClick = () => {
-//     // Логика для обработки входа
-//     console.log("Вход");
-//   };
-
-//   const handleRegistrationClick = () => {
-//     // Логика для обработки регистрации
-//     console.log("Регистрация");
-//   };
-
-//   return (
-//     <div className="menu-container">
-//       {showApp ? (
-//         <App />
-//       ) : (
-//         <>
-//           <div className="header">
-//             <button className="link" onClick={handleLoginClick}>
-//               ВХОД
-//             </button>
-//             <button className="link" onClick={handleRegistrationClick}>
-//               РЕГИСТРАЦИЯ
-//             </button>
-//           </div>
-//           <div>
-//             <div className="content">
-//               {/* <h1 className="title">BANSYS</h1> */}
-//               <img src={bansys} alt="bans"></img>
-//               <p className="subtitle">Bansys - Банкоматы - Терминалы</p>
-//             </div>
-//           </div>
-//           <div className="footer">
-//             {/* <button className="shop-button" onClick={handleShopButtonClick}>
-//               ЗА ПОКУПКАМИ
-//             </button> */}
-//             <img
-//               src={toBuy}
-//               alt="toBuy"
-//               className="shop-button"
-//               onClick={handleShopButtonClick}
-//             ></img>
-//           </div>
-//         </>
-//       )}
-//     </div>
-//   );
-// }
-
-// export default Main;
-import React from "react";
-import { Routes, Route, Link } from "react-router-dom"; // Импортируем необходимые компоненты
+import React, { useState } from "react";
+import { Routes, Route, useNavigate } from "react-router-dom";
 import App from "./App";
-import "./Main.css"; 
+import "./Main.css";
 import bansys from "./bansys.png";
 import toBuy from "./tobuy.png";
 
 function Main() {
-  return (
-    <div className="menu-container">
-      <div className="header">
-        <Link className="link" to="/login">
-          ВХОД
-        </Link>
-        <Link className="link" to="/registration">
-          РЕГИСТРАЦИЯ
-        </Link>
-      </div>
-      <div>
-        <div className="content">
-          <img src={bansys} alt="bans" />
-          <p className="subtitle">Bansys - Банкоматы - Терминалы</p>
-        </div>
-      </div>
-      <div className="footer">
-        <Link className="shop-button" to="/app">
-          <img src={toBuy} alt="toBuy" />
-          ЗА ПОКУПКАМИ
-        </Link>
-      </div>
+  const navigate = useNavigate(); // Используем хук для навигации
+  const handleLoginClick = () => {
+    console.log("Вход");
+  };
+  
+  const handleRegistrationClick = () => {
+    console.log("Регистрация");
+  };
 
-      <Routes>
-        {/* Определение маршрутов */}
-        <Route path="/app" element={<App />} />
-        <Route path="/login" element={<div>Форма входа</div>} />
-        <Route path="/registration" element={<div>Форма регистрации</div>} />
-      </Routes>
-    </div>
+  const handleShopButtonClick = () => {
+    navigate('/app'); // Переход на новую страницу
+  };
+
+  return (
+    <Routes>
+      <Route path="/" element={
+        <div className="menu-container">
+          <div className="header">
+            <button className="link" onClick={handleLoginClick}>
+              ВХОД
+            </button>
+            <button className="link" onClick={handleRegistrationClick}>
+              РЕГИСТРАЦИЯ
+            </button>
+          </div>
+          <div>
+            <div className="content">
+              <img src={bansys} alt="bans"></img>
+              <p className="subtitle">Bansys - Банкоматы - Терминалы</p>
+            </div>
+          </div>
+          <div className="footer">
+            <img
+              src={toBuy}
+              alt="toBuy"
+              className="shop-button"
+              onClick={handleShopButtonClick}
+            ></img>
+          </div>
+        </div>
+      } />
+      <Route path="/app" element={<App />} />
+    </Routes>
   );
 }
 
--- src/index.js
+++ src/index.js
@@ -1,27 +1,11 @@
-// import React from "react";
-// import ReactDOM from "react-dom/client";
-// import "./index.css";
-// import reportWebVitals from "./reportWebVitals";
-// import Main from "./Main";
-
-// const root = ReactDOM.createRoot(document.getElementById("root"));
-// root.render(
-//   <React.StrictMode>
-//     <Main />
-//   </React.StrictMode>
-// );
-// reportWebVitals();
-
-
 import React from "react";
 import ReactDOM from "react-dom/client";
-import { BrowserRouter } from "react-router-dom"; // Импортируем BrowserRouter
+import { BrowserRouter } from "react-router-dom";
 import "./index.css";
 import reportWebVitals from "./reportWebVitals";
 import Main from "./Main";
 
 const root = ReactDOM.createRoot(document.getElementById("root"));
-
 root.render(
   <React.StrictMode>
     <BrowserRouter>
@@ -29,5 +13,6 @@ root.render(
     </BrowserRouter>
   </React.StrictMode>
 );
-
 reportWebVitals();
+
+
