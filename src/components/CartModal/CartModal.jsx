51b87142d607bee21097e9320f7559c725f6d0c4
Timchin-cpu <83727119+Timchin-cpu@users.noreply.github.com>
2024-09-28 17:56:44 +0000

    modified:   src/components/CartModal/CartModal.jsx

--- src/components/CartModal/CartModal.jsx
+++ src/components/CartModal/CartModal.jsx
@@ -2,38 +2,7 @@ import React, { useEffect } from "react";
 import "./CartModal.css"; // Импортируем стили
 
 const CartModal = ({ items = [], onClose }) => {
-  useEffect(() => {
-    // Отключение прокрутки
-    document.body.classList.add("no-scroll");
-    return () => {
-      // Включение прокрутки при размонтировании
-      document.body.classList.remove("no-scroll");
-    };
-  }, []);
-
-  if (!items.length) return null; // Если корзина пуста, ничего не отображаем
-
-  return (
-    <div className="modal-overlay">
-      <div className="modal">
-        <button className="modal-close-button" onClick={onClose}>
-          <span className="icon">×</span>
-        </button>
-        <div className="modal-inner-content">
-          <h2>Ваша корзина</h2>
-          {items.map((item) => (
-            <div key={item.id} className="cart-item">
-              <img src={item.photo_url} alt={item.name} />
-              <span>{item.name}</span>
-              <span>{item.quantity} шт.</span>
-            </div>
-          ))}
-
-          {/* <button onClick={onClose}>Закрыть</button> */}
-        </div>
-      </div>
-    </div>
-  );
+  return <div></div>;
 };
 
 export default CartModal;
