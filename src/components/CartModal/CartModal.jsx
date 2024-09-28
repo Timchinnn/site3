e8404f78a0b67d42e3c7def4805ff77f362f4d7c
Timchin-cpu <83727119+Timchin-cpu@users.noreply.github.com>
2024-09-28 17:57:05 +0000

    modified:   src/components/CartModal/CartModal.jsx

--- src/components/CartModal/CartModal.jsx
+++ src/components/CartModal/CartModal.jsx
@@ -1,4 +1,4 @@
-import React, { useEffect } from "react";
+// import React, { useEffect } from "react";
 import "./CartModal.css"; // Импортируем стили
 
 const CartModal = ({ items = [], onClose }) => {
