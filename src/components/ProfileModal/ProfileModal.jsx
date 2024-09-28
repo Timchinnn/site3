cd50f1f2a2b1f4619426badb736fd35914377797
Timchin-cpu <83727119+Timchin-cpu@users.noreply.github.com>
2024-09-28 12:14:52 +0000

    modified:   src/components/ProfileModal/ProfileModal.css 	modified:   src/components/ProfileModal/ProfileModal.jsx

--- src/components/ProfileModal/ProfileModal.jsx
+++ src/components/ProfileModal/ProfileModal.jsx
@@ -3,25 +3,32 @@ import "./ProfileModal.css";
 // import buy from "./buy.png";
 // import cartButton from "./cartButton.png";
 import arrow from "./arrow.png";
-import b from './B.png'
-import car from './car.png'
+import b from "./B.png";
+import car from "./car.png";
 
 const ProfileModal = ({ onClose }) => {
   return (
     <div className="modal-overlay">
       <div className="modal-content">
         <img className="arrow" src={arrow} alt="arrow"></img>
-        
-        
+
         <div className="buttonsMainProfile">
           {/* <img src={buy} alt="buy"></img>
           <img src={cartButton} alt="cartButton"></img> */}
-          <button><img src={b} alt='b' className="b"></img></button>
-          <button><img src={b} alt='b' className="b"></img></button>
-          <button><img src={b} alt='b' className="b"></img></button>
-          <button><img src={b} alt='b' className="b"></img></button>
+          <button>
+            <img src={b} alt="b" className="b"></img>
+          </button>
+          <button>
+            <img src={b} alt="b" className="b"></img>
+          </button>
+          <button>
+            <img src={b} alt="b" className="b"></img>
+          </button>
+          {/* <button><img src={b} alt='b' className="b"></img></button> */}
         </div>
-        <button className="delivery"><img src={car} alt='car' className="car"></img></button>
+        <button className="delivery">
+          <img src={car} alt="car" className="car"></img>
+        </button>
       </div>
     </div>
   );
