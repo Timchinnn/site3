a98c69508075c3f63e36a0c8f2b68a453d4383f0
Timchin-cpu <83727119+Timchin-cpu@users.noreply.github.com>
2024-09-28 18:01:45 +0000

    modified:   src/components/CartModal/CartModal.jsx

--- src/components/CartModal/CartModal.jsx
+++ src/components/CartModal/CartModal.jsx
@@ -1,8 +1,32 @@
-// import React, { useEffect } from "react";
-import "./CartModal.css"; // Импортируем стили
+import React from "react";
+import "./CartModal.css"; // Import styles
 
 const CartModal = ({ items = [], onClose }) => {
-  return <div></div>;
+  return (
+    <div className="modal-overlay">
+      <div className="modal-content">
+        <button className="modal-close-button" onClick={onClose}>
+          <span className="icon">×</span>
+        </button>
+        <h2>Корзина</h2>
+        {items.length === 0 ? (
+          <p>Ваша корзина пуста</p>
+        ) : (
+          <ul>
+            {items.map((item) => (
+              <li key={item.id} className="cart-item">
+                <img src={item.photo_url} alt={item.name} />
+                <div>
+                  <p>{item.name}</p>
+                  <p>Количество: {item.quantity}</p>
+                </div>
+              </li>
+            ))}
+          </ul>
+        )}
+      </div>
+    </div>
+  );
 };
 
 export default CartModal;
