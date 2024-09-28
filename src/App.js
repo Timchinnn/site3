bb8fe3e636a612e5f9ec3606a995400dd9ea9fbc
Timchin-cpu <83727119+Timchin-cpu@users.noreply.github.com>
2024-09-28 17:22:44 +0000

    modified:   src/App.js 	modified:   src/components/ProductModal/ProductModal.jsx

--- src/App.js
+++ src/App.js
@@ -1,38 +1,43 @@
 import { useState, useEffect } from "react";
 import axios from "axios";
-// import { useNavigate } from "react-router-dom";
+import { useNavigate } from "react-router-dom"; // Импортируем useNavigate
 import Search from "./components/Search/Search";
-import ProductModal from "./components/ProductModal/ProductModal";
-import CartModal from "./components/CartModal/CartModal";
-import useCart from "./useCart";
 import "./App.css";
-import { getTotalPrice } from "./utils";
+import fly from "./fly.png";
+import myLog from "./myLog.png";
+import sendRequest from "./sendRequest.png";
+import dn from "./dn.png";
+import hyosung from "./hyosung.png";
+import ncr from "./ncr.png";
 import cart from "./cart.png";
+import ProductModal from "./components/ProductModal/ProductModal";
 function App() {
-  // const navigate = useNavigate();
+  const navigate = useNavigate(); // Используем хук для навигации
   const [products, setProducts] = useState([]);
   const [categories, setCategories] = useState([]);
   const [searchQuery, setSearchQuery] = useState("");
   const [selectedProduct, setSelectedProduct] = useState(null);
   const [isProductModalOpen, setIsProductModalOpen] = useState(false);
-  const [isCartModalOpen, setIsCartModalOpen] = useState(false);
-
-  const { addedItems, onAdd, onRemove } = useCart();
-
   const fetchCategories = () => {
     axios
       .get("/api/categories")
-      .then((response) => setCategories(response.data))
-      .catch((error) =>
-        console.error("Ошибка при получении категорий:", error)
-      );
+      .then((response) => {
+        setCategories(response.data);
+      })
+      .catch((error) => {
+        console.error("Ошибка при получении категорий:", error);
+      });
   };
 
   const fetchProducts = () => {
     axios
       .get("/api/products")
-      .then((response) => setProducts(response.data))
-      .catch((error) => console.error("Ошибка при получении товаров:", error));
+      .then((response) => {
+        setProducts(response.data);
+      })
+      .catch((error) => {
+        console.error("Ошибка при получении товаров:", error);
+      });
   };
 
   useEffect(() => {
@@ -44,6 +49,18 @@ function App() {
     product.name.toLowerCase().includes(searchQuery.toLowerCase())
   );
 
+  const openProfilePage = () => {
+    navigate("/profile"); // Переход на страницу профиля
+  };
+
+  const openSendRequestPage = () => {
+    navigate("/send-request"); // Переход на страницу отправки запроса
+  };
+
+  // const openProductPage = (product) => {
+  //   console.log(product)
+  //   navigate(`/product/${product.id}`); // Переход на страницу продукта
+  // };
   const openProductModal = (product) => {
     setSelectedProduct(product);
     setIsProductModalOpen(true);
@@ -53,18 +70,39 @@ function App() {
     setSelectedProduct(null);
     setIsProductModalOpen(false);
   };
-
-  const openCartModal = () => {
-    setIsCartModalOpen(true);
-  };
-
-  const closeCartModal = () => {
-    setIsCartModalOpen(false);
-  };
-
   return (
     <div className="main">
+      <div className="header-name">
+        <div className="tg-link-button">
+          <img src={fly} alt="tglink" className="tglink"></img>
+          <a href="https://t.me/Bansys_sale" className="tg-button">
+            @Bansys_sale
+          </a>
+        </div>
+      </div>
       <Search searchQuery={searchQuery} setSearchQuery={setSearchQuery} />
+      <div className="about-buttons-question">
+        <div className="why">Почему BANSYS?</div>
+        <div className="how">Как купить?</div>
+        <div className="garante">Гарантия</div>
+        <div className="loyal">Наша программа лояльности</div>
+      </div>
+      <div className="log-help">
+        <img
+          src={myLog}
+          className="my-log"
+          alt=""
+          loading="eager"
+          onClick={openProfilePage}
+        ></img>
+        <img src={sendRequest} alt="" onClick={openSendRequestPage}></img>
+      </div>
+      <div className="company">
+        <img src={hyosung} alt="hyosung" className="hyosung"></img>
+        <img src={dn} alt="dn" className="dn"></img>
+        <img src={ncr} alt="ncr"></img>
+      </div>
+      <h1 className="catalog">Каталог</h1>
       <div className="category">
         {categories.length > 0 ? (
           categories.map((category) => (
@@ -77,7 +115,7 @@ function App() {
                     <div
                       key={product.id}
                       className="product-item"
-                      onClick={() => openProductModal(product)}
+                      onClick={() => openProductModal(product)} // Переход на страницу продукта
                     >
                       <img
                         src={product.photo_url}
@@ -87,12 +125,7 @@ function App() {
                       <p className="product-name">{product.name}</p>
                       <div className="ordertext-cart">
                         <p>Под заказ</p>
-                        <img
-                          src={cart}
-                          alt="cart"
-                          className="img-cart"
-                          onClick={openCartModal}
-                        />
+                        <img src={cart} alt={cart} className="img-cart" />
                       </div>
                     </div>
                   ))}
@@ -104,20 +137,7 @@ function App() {
         )}
       </div>
       {isProductModalOpen && (
-        <ProductModal
-          product={selectedProduct}
-          onClose={closeProductModal}
-          onAdd={onAdd}
-          onRemove={onRemove}
-          addedItems={addedItems}
-        />
-      )}
-      {isCartModalOpen && (
-        <CartModal
-          items={addedItems}
-          total={getTotalPrice(addedItems)}
-          onClose={closeCartModal}
-        />
+        <ProductModal product={selectedProduct} onClose={closeProductModal} />
       )}
     </div>
   );
