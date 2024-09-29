58938d2c8ecfe6d541137fee3794a2c9e93a95a6
ArtFullstack <ve4tom@gmail.com>
2024-09-29 13:58:19 +0000

    modified:   src/components/SendRequestModal/SendRequestModal.jsx

--- src/components/SendRequestModal/SendRequestModal.jsx
+++ src/components/SendRequestModal/SendRequestModal.jsx
@@ -3,15 +3,16 @@ import "./SendRequestModal.css"; // Добавьте стили, если нео
 import bigB from './bigb.png'
 import bigBContray from './bigbContrary.png'
 import arrow from './arrow.png'
-
+import { useNavigate } from "react-router-dom";
 const SendRequestModal = ({ onClose }) => {
+  const navigate = useNavigate();
   return (
     <div className="modal-overlay1">
       <div className="images-container">
         <img src={bigB} alt="bigB" className="top-left" />
         <img src={bigBContray} alt="bigBContray" className="bottom-right" />
         <div className="parent">
-          <img className="arrow" src={arrow} alt="arrow" />
+          <img className="arrow" src={arrow} alt="arrow" onClick={() => navigate(-1)}/>
           <div className="window-reg">
             <h1 className="application">ЗАЯВКА</h1>
             <div className="input-container">
