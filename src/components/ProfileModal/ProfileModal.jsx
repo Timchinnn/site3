0acee9ea299f6b7d0c9dcb17653e9ea4384cc124
ArtFullstack <ve4tom@gmail.com>
2024-09-29 13:55:45 +0000

    modified:   src/components/ProfileModal/ProfileModal.jsx

--- src/components/ProfileModal/ProfileModal.jsx
+++ src/components/ProfileModal/ProfileModal.jsx
@@ -5,12 +5,13 @@ import "./ProfileModal.css";
 import arrow from "./arrow.png";
 import b from "./B.png";
 import car from "./car.png";
-
+import { useNavigate } from "react-router-dom"
 const ProfileModal = ({ onClose }) => {
+  const navigate = useNavigate();
   return (
     <div className="modal-overlay">
       <div className="modal-content">
-        <img className="arrow" src={arrow} alt="arrow"></img>
+        <img className="arrow" src={arrow} alt="arrow" onClick={() => navigate(-1)}></img>
 
         <div className="buttonsMainProfile">
           {/* <img src={buy} alt="buy"></img>
