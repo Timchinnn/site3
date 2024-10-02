06e816e9e3453f4668ee7795a976ecf059cadbba
ArtFullstack <ve4tom@gmail.com>
2024-10-02 16:55:13 +0000

    modified:   src/components/ProfileModal/ProfileModal.jsx

--- src/components/ProfileModal/ProfileModal.jsx
+++ src/components/ProfileModal/ProfileModal.jsx
@@ -5,13 +5,18 @@ import "./ProfileModal.css";
 import arrow from "./arrow.png";
 import b from "./B.png";
 import car from "./car.png";
-import { useNavigate } from "react-router-dom"
+import { useNavigate } from "react-router-dom";
 const ProfileModal = ({ onClose }) => {
   const navigate = useNavigate();
   return (
     <div className="modal-overlay">
       <div className="modal-content">
-        <img className="arrow" src={arrow} alt="arrow" onClick={() => navigate(-1)}></img>
+        <img
+          className="arrow"
+          src={arrow}
+          alt="arrow"
+          onClick={() => navigate(-1)}
+        ></img>
 
         <div className="buttonsMainProfile">
           {/* <img src={buy} alt="buy"></img>
@@ -22,9 +27,7 @@ const ProfileModal = ({ onClose }) => {
           <button>
             <img src={b} alt="b" className="b"></img>
           </button>
-          <button>
-            <img src={b} alt="b" className="b"></img>
-          </button>
+          <button>{/* <img src={b} alt="b" className="b"></img> */}</button>
           {/* <button><img src={b} alt='b' className="b"></img></button> */}
         </div>
         <button className="delivery">
