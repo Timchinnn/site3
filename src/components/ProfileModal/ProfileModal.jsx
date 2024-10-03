b79f6952cf9acd16211d648e98c7b2ba097eabe9
ArtFullstack <ve4tom@gmail.com>
2024-10-03 14:09:08 +0000

    modified:   src/components/ProfileModal/ProfileModal.jsx

--- src/components/ProfileModal/ProfileModal.jsx
+++ src/components/ProfileModal/ProfileModal.jsx
@@ -1,4 +1,4 @@
-import React from "react";
+import React, { useState } from "react";
 import "./ProfileModal.css";
 // import buy from "./buy.png";
 // import cartButton from "./cartButton.png";
@@ -8,65 +8,79 @@ import car from "./car.png";
 import book from "./book.png";
 import { useNavigate } from "react-router-dom";
 const ProfileModal = ({ onClose }) => {
+  const [isModalVisible, setIsModalVisible] = useState(false);
+
+  const handleButtonClick = () => {
+    setIsModalVisible(true);
+  };
   const navigate = useNavigate();
   return (
-    <div className="modal-overlay">
-      <div className="modal-content">
-        <img
-          className="arrow"
-          src={arrow}
-          alt="arrow"
-          onClick={() => navigate(-1)}
-        ></img>
+    <div>
+      {!isModalVisible ? (
+        <div className="modal-overlay">
+          <div className="modal-content">
+            <img
+              className="arrow"
+              src={arrow}
+              alt="arrow"
+              onClick={() => navigate(-1)}
+            ></img>
 
-        <div className="buttonsMainProfile">
-          <button>
-            <div className="buy-about">
-              <img src={book} alt="book" />
-              <div className="buy-and-more">
-                <p>Покупки</p>
-                <p>Подробнее</p>
-              </div>
+            <div className="buttonsMainProfile">
+              <button onClick={handleButtonClick}>
+                <div className="buy-about">
+                  <img src={book} alt="book" />
+                  <div className="buy-and-more">
+                    <p>Покупки</p>
+                    <p>Подробнее</p>
+                  </div>
+                </div>
+                <img src={b} alt="b" className="b"></img>
+              </button>
+              <button>
+                <div className="buy-about">
+                  <p>3%</p>
+                  <div className="buy-and-more">
+                    <p>Скидка</p>
+                    <p>Сумма выкупа</p>
+                    <p>Подробнее</p>
+                  </div>
+                </div>
+                <img src={b} alt="b" className="b"></img>
+              </button>
+              <button>
+                <img alt="user-photo" />
+                <div>
+                  <p>Иван Владимирович </p>
+                  <div>
+                    <p>Телефон</p>
+                    <p></p>
+                  </div>
+                  <div>
+                    <p>Email</p>
+                    <p></p>
+                  </div>
+                </div>
+                <p>Посмотреть все данные</p>
+              </button>
             </div>
-            <img src={b} alt="b" className="b"></img>
-          </button>
-          <button>
-            <div className="buy-about">
-              <p>3%</p>
-              <div className="buy-and-more">
-                <p>Скидка</p>
-                <p>Сумма выкупа</p>
-                <p>Подробнее</p>
-              </div>
-            </div>
-            <img src={b} alt="b" className="b"></img>
-          </button>
-          <button>
-            <img alt="user-photo" />
-            <div>
-              <p>Иван Владимирович </p>
+            <button className="delivery">
+              <p>Доставка</p>
+              <p>Ближайшая: не ожидается</p>
               <div>
-                <p>Телефон</p>
-                <p></p>
+                <button>Подробнее</button>
+                <button>Пожаловаться на доставку</button>
               </div>
-              <div>
-                <p>Email</p>
-                <p></p>
-              </div>
-            </div>
-            <p>Посмотреть все данные</p>
-          </button>
-        </div>
-        <button className="delivery">
-          <p>Доставка</p>
-          <p>Ближайшая: не ожидается</p>
-          <div>
-            <button>Подробнее</button>
-            <button>Пожаловаться на доставку</button>
+              <img src={car} alt="car" className="car"></img>
+            </button>
           </div>
-          <img src={car} alt="car" className="car"></img>
-        </button>
-      </div>
+        </div>
+      ) : (
+        <div className="modal-content">
+          <p>Новое содержимое страницы</p>
+          <button onClick={() => setIsModalVisible(false)}>Назад</button>
+        </div>
+      )}
     </div>
   );
 };
