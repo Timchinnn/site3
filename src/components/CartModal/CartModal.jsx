8fceac910a24d7f64242f9b2ddb6de4b7c89c58a
ArtFullstack <ve4tom@gmail.com>
2024-10-07 19:07:07 +0000

    modified:   src/components/CartModal/CartModal.jsx

--- src/components/CartModal/CartModal.jsx
+++ src/components/CartModal/CartModal.jsx
@@ -26,6 +26,7 @@ const CartModal = ({ items = [], onClose, onAdd, onRemove, onResetCart }) => {
   const handleOk = () => {
     onResetCart(); // Сбрасываем содержимое корзины
     onClose(); // Закрываем модальное окно
+    window.location.reload();
   };
 
   return (
