89e4b961072c2f475cff556b33a964e93eb6b649
ArtFullstack <ve4tom@gmail.com>
2024-10-07 09:19:24 +0000

    modified:   src/components/GuaranteePage/GuaranteePage.jsx 	new file:   src/components/GuaranteePage/arrow.png 	new file:   src/components/GuaranteePage/bigb.png 	new file:   src/components/GuaranteePage/bigbContrary.png 	modified:   src/components/HowToBuyPage/HowToBuyPage.jsx 	new file:   src/components/HowToBuyPage/arrow.png 	new file:   src/components/HowToBuyPage/bigb.png 	new file:   src/components/HowToBuyPage/bigbContrary.png 	modified:   src/components/LoyaltyProgramPage/LoyaltyProgramPage.jsx 	new file:   src/components/LoyaltyProgramPage/arrow.png 	new file:   src/components/LoyaltyProgramPage/bigb.png 	new file:   src/components/LoyaltyProgramPage/bigbContrary.png 	modified:   src/components/WhyBansysPage/WhyBansysPage.jsx 	new file:   src/components/WhyBansysPage/arrow.png 	new file:   src/components/WhyBansysPage/bigb.png 	new file:   src/components/WhyBansysPage/bigbContrary.png

---
+++ src/components/GuaranteePage/GuaranteePage.jsx
@@ -0,0 +1,30 @@
+import React from "react";
+
+import "./GuaranteePage.css";
+import bigB from "./bigb.png";
+import bigBContray from "./bigbContrary.png";
+import arrow from "./arrow.png";
+import { useNavigate } from "react-router-dom";
+
+const GuaranteePage = ({ onClose }) => {
+  const navigate = useNavigate();
+
+  return (
+    <div className="modal-overlay1">
+      <div className="images-container">
+        <img src={bigB} alt="bigB" className="top-left" />
+        <img src={bigBContray} alt="bigBContray" className="bottom-right" />
+        <div className="parent">
+          <img
+            className="arrow"
+            src={arrow}
+            alt="arrow"
+            onClick={() => navigate(-1)}
+          />
+        </div>
+      </div>
+    </div>
+  );
+};
+
+export default GuaranteePage;
