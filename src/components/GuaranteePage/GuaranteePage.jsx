737243bfe67fe3f1aa43b9447f5b912e7da850b1
Timchin-cpu <83727119+Timchin-cpu@users.noreply.github.com>
2024-10-10 09:43:14 +0000

    modified:   src/components/GuaranteePage/GuaranteePage.jsx 	new file:   src/components/GuaranteePage/arrow-long.png 	new file:   src/components/GuaranteePage/arrowDownLong.png 	new file:   src/components/GuaranteePage/arrowDownM.png 	new file:   src/components/GuaranteePage/arrowDownS.png 	new file:   src/components/GuaranteePage/arrows.png

--- src/components/GuaranteePage/GuaranteePage.jsx
+++ src/components/GuaranteePage/GuaranteePage.jsx
@@ -5,6 +5,11 @@ import bigB from "./bigb.png";
 import bigBContray from "./bigbContrary.png";
 import arrow from "./arrow.png";
 import { useNavigate } from "react-router-dom";
+import arrowLong from './arrow-long.png'
+import arrowDownL from './arrowDownLong.png'
+import arrowDownM from './arrowDownM.png'
+import arrowDownS from './arrowDownS.png'
+import arrowXs from './arrows.png'
 
 const GuaranteePage = ({ onClose }) => {
   const navigate = useNavigate();
@@ -21,7 +26,53 @@ const GuaranteePage = ({ onClose }) => {
             alt="arrow"
             onClick={() => navigate(-1)}
           />
-          <div className="window-block-about"></div>
+          {/* <div className="window-block-about"></div> */}
+          <div>
+            <p>Зови друзей </p>
+            <div>
+              <img src={arrowLong} alt="arrowLong" />
+              <p>и зарабатывай</p>
+            </div>
+            <hr />
+            <p>Получайте 10% от каждого заказа по твоей рекомендации!
+            </p>
+            <p>Как это работает:</p>
+            <div>
+              <div>
+                <p>Получайте уникальную ссылку — нажми на кнопку ниже и скопируйте свою персональную ссылку.</p>
+              </div>
+              <div>
+                <p>Делитесь с друзьями — отправляйте ссылку коллегам, партнёрам и знакомым в мессенджерах или социальных сетях.</p>
+              </div>
+              <div>
+                <p>Зарабатывайте вместе с нами — когда по вашей ссылке оформят заказ, вы получите 10% от первого чека.</p>
+              </div>
+            </div>
+            <hr />
+            <p>Каждый месяц самые активные участники получают ценные призы:</p>
+            <div>
+              <div><img src={arrowDownL} alt="arrowDownL" />
+              <p>1 место: скидка 15%</p></div>
+              <div><img src={arrowDownM} alt="arrowDownM" />
+              <p>2 место: скидка 10%</p></div>
+              <div><img src={arrowDownS} alt="arrowDownS" />
+              <p>3 место: скидка 3%</p></div>
+              
+            </div>
+            <div>
+              <p>Статистика в реальном времени — отслеживайте количество переходов 
+              и заработанных денег в личном кабинете.</p>
+            </div>
+            <p>Нажмите на кнопку ниже, чтобы получить свою уникальную ссылку и начать зарабатывать уже сегодня.</p>
+            <img src={arrowXs} alt="arrowXs" />
+            <p> [Получить ссылку и начать зарабатывать]</p>
+            <div>
+              <p>Твоя ссылка на приглашение</p>
+              <p>https://t.me......................</p>
+              <button>Скопировать ссылку</button>
+              <p></p>
+            </div>
+          </div>
         </div>
       </div>
     </div>
