bd7123eb084115b1b43e31df9ebcabc0ee7c571f
Timchin-cpu <83727119+Timchin-cpu@users.noreply.github.com>
2024-10-13 20:41:53 +0000

    modified:   src/components/ProfileModal/ProfileModal.css 	modified:   src/components/ProfileModal/ProfileModal.jsx

--- src/components/ProfileModal/ProfileModal.jsx
+++ src/components/ProfileModal/ProfileModal.jsx
@@ -10,6 +10,7 @@ const ProfileModal = ({ onClose }) => {
   const navigate = useNavigate();
   const [products, setProducts] = useState([]);
   const [activeItem, setActiveItem] = useState(null);
+  const [isModalOpen, setIsModalOpen] = useState(false);
 
   useEffect(() => {
     axios
@@ -18,53 +19,65 @@ const ProfileModal = ({ onClose }) => {
       .catch((error) => console.error("Ошибка при получении товаров:", error));
   }, []);
 
+  const openModal = () => {
+    setIsModalOpen(true);
+  };
+
+  const closeModal = () => {
+    setIsModalOpen(false);
+  };
+
   return (
     <div>
-      <div className="modal-overlay">
-        <div className="modal-content">
-          <img
-            className="arrow"
-            src={arrow}
-            alt="arrow"
-            onClick={() => navigate(-1)}
-          />
-          <div className="product-names">
-            {products.map((product) => (
-              <div
-                key={product.id}
-                className={`product-name-item ${
-                  activeItem === product.id ? "active" : ""
-                }`}
-                onClick={() => setActiveItem(product.id)}
-              >
-                <div className="product-main-content">
-                  <p>{product.name}</p>
-                  <img src={arrowDown} alt="arrowDown" />
-                </div>
-                {activeItem === product.id && (
-                  <div className="additional-elements">
-                    <div className="input-container1">
-                      <input
-                        type="text"
-                        className="input-field"
-                        placeholder="Введите текст..."
-                      />
-                      <div className="highlight">
-                        <p>10</p>
-                        <img src={union} alt="union" />
-                      </div>
-                    </div>
-
-                    <button className="submit-button">
-                      сделать предложение
-                    </button>
+      <button onClick={openModal}>Открыть модальное окно</button>
+      {isModalOpen && (
+        <div className="modal-overlay">
+          <div className="modal-content">
+            <img
+              className="arrow"
+              src={arrow}
+              alt="arrow"
+              onClick={() => navigate(-1)}
+            />
+            <div className="product-names">
+              {products.map((product) => (
+                <div
+                  key={product.id}
+                  className={`product-name-item ${
+                    activeItem === product.id ? "active" : ""
+                  }`}
+                  onClick={() => setActiveItem(product.id)}
+                >
+                  <div className="product-main-content">
+                    <p>{product.name}</p>
+                    <img src={arrowDown} alt="arrowDown" />
                   </div>
-                )}
-              </div>
-            ))}
+                  {activeItem === product.id && (
+                    <div className="additional-elements">
+                      <div className="input-container1">
+                        <input
+                          type="text"
+                          className="input-field"
+                          placeholder="Введите текст..."
+                        />
+                        <div className="highlight">
+                          <p>10</p>
+                          <img src={union} alt="union" />
+                        </div>
+                      </div>
+
+                      <button className="submit-button">
+                        сделать предложение
+                      </button>
+                    </div>
+                  )}
+                </div>
+              ))}
+            </div>
+            <button onClick={closeModal}>Закрыть</button>
           </div>
         </div>
-      </div>
+      )}
     </div>
   );
 };
