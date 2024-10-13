dd6c850e73b19b5911433d6489cd5f7ea69a97a0
Timchin-cpu <83727119+Timchin-cpu@users.noreply.github.com>
2024-10-13 10:02:42 +0000

    modified:   src/App.js

--- src/App.js
+++ src/App.js
@@ -92,7 +92,9 @@ function App() {
       <Search searchQuery={searchQuery} setSearchQuery={setSearchQuery} />
       <div className="about-buttons-question">
         <div className="why" onClick={() => navigate("/why-bansys")}>
-          Почему<span className="bansys-highlight">BANSYS?</span>
+          Почему
+          <br />
+          <span className="bansys-highlight">BANSYS?</span>
         </div>
         <div className="how" onClick={() => navigate("/how-to-buy")}>
           Поделись с другом
