2588eab8f0344f960932d22ac5fae2f7f04efa5d
Timchin-cpu <83727119+Timchin-cpu@users.noreply.github.com>
2024-10-13 20:50:55 +0000

    modified:   src/components/ProfileModal/ProfileModal.jsx

--- src/components/ProfileModal/ProfileModal.jsx
+++ src/components/ProfileModal/ProfileModal.jsx
@@ -29,51 +29,56 @@ const ProfileModal = ({ onClose }) => {
 
   return (
     <div>
-      <button onClick={openModal}>Открыть модальное окно</button>
-      {isModalOpen && (
-        <div className="modal-overlay">
-          <div className="modal-content">
-            <img
-              className="arrow"
-              src={arrow}
-              alt="arrow"
-              onClick={() => navigate(-1)}
-            />
-            <div className="product-names">
-              {products.map((product) => (
-                <div
-                  key={product.id}
-                  className={`product-name-item ${
-                    activeItem === product.id ? "active" : ""
-                  }`}
-                  onClick={() => setActiveItem(product.id)}
-                >
-                  <div className="product-main-content">
-                    <p>{product.name}</p>
-                    <img src={arrowDown} alt="arrowDown" />
-                  </div>
-                  {activeItem === product.id && (
-                    <div className="additional-elements">
-                      <div className="input-container1">
-                        <input
-                          type="text"
-                          className="input-field"
-                          placeholder="Введите текст..."
-                        />
-                        <div className="highlight">
-                          <p>10</p>
-                          <img src={union} alt="union" />
-                        </div>
-                      </div>
-
-                      <button className="submit-button">
-                        сделать предложение
-                      </button>
-                    </div>
-                  )}
+      <div className="modal-overlay">
+        <div className="modal-content">
+          <img
+            className="arrow"
+            src={arrow}
+            alt="arrow"
+            onClick={() => navigate(-1)}
+          />
+          <div className="product-names">
+            {products.map((product) => (
+              <div
+                key={product.id}
+                className={`product-name-item ${
+                  activeItem === product.id ? "active" : ""
+                }`}
+                onClick={() => setActiveItem(product.id)}
+              >
+                <div className="product-main-content">
+                  <p>{product.name}</p>
+                  <img src={arrowDown} alt="arrowDown" />
                 </div>
-              ))}
-            </div>
+                {activeItem === product.id && (
+                  <div className="additional-elements">
+                    <div className="input-container1">
+                      <input
+                        type="text"
+                        className="input-field"
+                        placeholder="Введите текст..."
+                      />
+                      <div className="highlight">
+                        <p>10</p>
+                        <img src={union} alt="union" />
+                      </div>
+                    </div>
+
+                    <button className="submit-button" onClick={openModal}>
+                      сделать предложение
+                    </button>
+                  </div>
+                )}
+              </div>
+            ))}
+          </div>
+        </div>
+      </div>
+      {isModalOpen && (
+        <div className="modal">
+          <div className="modal-content">
+            <h2>Модальное окно</h2>
+            <p>Содержимое модального окна</p>
             <button onClick={closeModal}>Закрыть</button>
           </div>
         </div>
