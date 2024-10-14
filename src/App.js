2e26d16863d965d0e00c5ec402071565acf94880
Timchin-cpu <83727119+Timchin-cpu@users.noreply.github.com>
2024-10-14 13:29:58 +0000

    modified:   src/App.css 	modified:   src/App.js

--- src/App.js
+++ src/App.js
@@ -244,7 +244,7 @@ function App() {
         )}
       </div>
       {cartItems.length > 0 && (
-        <button className="cartgl" onClick={openCartModal}>
+        <button className="cart-button" onClick={openCartModal}>
           Корзина ({cartItems.length})
         </button>
       )}
