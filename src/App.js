838dbae0ebe2f2a634e937e93f2e8bf5795cfcac
Timchin-cpu <83727119+Timchin-cpu@users.noreply.github.com>
2024-10-23 16:10:25 +0000

    modified:   src/App.js

--- src/App.js
+++ src/App.js
@@ -110,11 +110,21 @@ function App() {
       <div className="log-help">
         <div className="my-log" onClick={() => navigate("/profile")}>
           <img src={left} alt="right"></img>
-          <p>Для поставщиков</p>
+          <p>Поделись с другом</p>
         </div>
         <div className="help" onClick={() => navigate("/send-request")}>
-          <p>Оставить заявку</p>
+          <p>Гарантия</p>
           <img src={right} alt="left"></img>
+        </div>
+        <div className="log-help">
+          <div className="my-log" onClick={() => navigate("/profile")}>
+            <img src={left} alt="right"></img>
+            <p>Для поставщиков</p>
+          </div>
+          <div className="help" onClick={() => navigate("/send-request")}>
+            <p>Оставить заявку</p>
+            <img src={right} alt="left"></img>
+          </div>
         </div>
         {/* <img
           src={myLog}
