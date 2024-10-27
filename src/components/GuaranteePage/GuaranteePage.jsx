612b10179b2c9ae805f2230da3c0806f4c0e25ed
Timchinnn <95688018+Timchinnn@users.noreply.github.com>
2024-10-27 16:17:23 +0000

    modified:   src/components/GuaranteePage/GuaranteePage.jsx 	modified:   src/locales/en.json 	modified:   src/locales/ru.json

--- src/components/GuaranteePage/GuaranteePage.jsx
+++ src/components/GuaranteePage/GuaranteePage.jsx
@@ -5,11 +5,12 @@ import bigB from "./bigb.png";
 import bigBContray from "./bigbContrary.png";
 import arrow from "./arrow.png";
 import { useNavigate } from "react-router-dom";
-import arrowLong from './arrow-long.png'
-import arrowDownL from './arrowDownLong.png'
-import arrowDownM from './arrowDownM.png'
-import arrowDownS from './arrowDownS.png'
-import arrowXs from './arrows.png'
+import arrowLong from "./arrow-long.png";
+import arrowDownL from "./arrowDownLong.png";
+import arrowDownM from "./arrowDownM.png";
+import arrowDownS from "./arrowDownS.png";
+import arrowXs from "./arrows.png";
+import { t } from "i18next";
 
 const GuaranteePage = ({ onClose }) => {
   const navigate = useNavigate();
@@ -28,61 +29,90 @@ const GuaranteePage = ({ onClose }) => {
           />
           {/* <div className="window-block-about"></div> */}
           <div className="share">
-            <p className="share-header-p">Зови друзей </p>
+            <p className="share-header-p">{t("Summoning friend")} </p>
             <div className="earn">
               <img src={arrowLong} alt="arrowLong" />
-              <p className="share-header-p">и зарабатывай</p>
+              <p className="share-header-p"></p>
             </div>
-            <hr className="hr1"/>
-            <p className="all-p1">Получайте 10% от каждого заказа по твоей рекомендации!
+            <hr className="hr1" />
+            <p className="all-p1">
+              {t(
+                "According to your suggestion, each order can enjoy a 3% discount!"
+              )}
             </p>
             <p className="all-p">Как это работает:</p>
             <div className="about-sales">
               <div className="about-sale">
-                <p>Получайте уникальную ссылку — нажми на кнопку ниже и скопируйте свою персональную ссылку.</p>
+                <p>
+                  Получайте уникальную ссылку — нажми на кнопку ниже и
+                  скопируйте свою персональную ссылку.
+                </p>
               </div>
               <div className="about-sale">
-                <p>Делитесь с друзьями — отправляйте ссылку коллегам, партнёрам и знакомым в мессенджерах или социальных сетях.</p>
+                <p>
+                  Делитесь с друзьями — отправляйте ссылку коллегам, партнёрам и
+                  знакомым в мессенджерах или социальных сетях.
+                </p>
               </div>
               <div className="about-sale">
-                <p>Зарабатывайте вместе с нами — когда по вашей ссылке оформят заказ, вы получите 10% от первого чека.</p>
+                <p>
+                  Зарабатывайте вместе с нами — когда по вашей ссылке оформят
+                  заказ, вы получите 10% от первого чека.
+                </p>
               </div>
             </div>
-            <hr className="hr2"/>
-            <p className="all-p">Каждый месяц самые активные участники получают ценные призы:</p>
+            <hr className="hr2" />
+            <p className="all-p">
+              Каждый месяц самые активные участники получают ценные призы:
+            </p>
             <div className="arrow-groups">
-              <div className="arrow-group"><img src={arrowDownL} alt="arrowDownL" />
-              <p>1 место: скидка 15%</p></div>
-              <div className="arrow-group"><img src={arrowDownM} alt="arrowDownM" />
-              <p>2 место: скидка 10%</p></div>
-              <div  className="arrow-group"><img src={arrowDownS} alt="arrowDownS" />
-              <p>3 место: скидка 3%</p></div>
-              
+              <div className="arrow-group">
+                <img src={arrowDownL} alt="arrowDownL" />
+                <p>1 место: скидка 15%</p>
+              </div>
+              <div className="arrow-group">
+                <img src={arrowDownM} alt="arrowDownM" />
+                <p>2 место: скидка 10%</p>
+              </div>
+              <div className="arrow-group">
+                <img src={arrowDownS} alt="arrowDownS" />
+                <p>3 место: скидка 3%</p>
+              </div>
             </div>
-            <div className="statistic-hrad"><div className="statistic">
-              <p className="all-p4">Статистика в реальном времени — отслеживайте количество переходов 
-              и заработанных денег в личном кабинете.</p>
-            </div></div>
-            
-            <p className="all-p2">Нажмите на кнопку ниже, чтобы получить свою уникальную ссылку и начать зарабатывать уже сегодня.</p>
-            
-            <div className="arrowxs"><img src={arrowXs} alt="arrowXs" /></div>
+            <div className="statistic-hrad">
+              <div className="statistic">
+                <p className="all-p4">
+                  Статистика в реальном времени — отслеживайте количество
+                  переходов и заработанных денег в личном кабинете.
+                </p>
+              </div>
+            </div>
+
+            <p className="all-p2">
+              Нажмите на кнопку ниже, чтобы получить свою уникальную ссылку и
+              начать зарабатывать уже сегодня.
+            </p>
+
+            <div className="arrowxs">
+              <img src={arrowXs} alt="arrowXs" />
+            </div>
             <p className="all-p2"> [Получить ссылку и начать зарабатывать]</p>
-            <div className="innovation-head">         <div className="invitation">
-              <p className="all-p">Твоя ссылка на приглашение</p>
-              <p className="all-p">https://t.me......................</p>
-              <button>Скопировать ссылку</button>
-              <div className="changeover">
-                <p className="all-p">Переходов по ссылке</p>
-                <p className="all-p">0</p>
+            <div className="innovation-head">
+              {" "}
+              <div className="invitation">
+                <p className="all-p">Твоя ссылка на приглашение</p>
+                <p className="all-p">https://t.me......................</p>
+                <button>Скопировать ссылку</button>
+                <div className="changeover">
+                  <p className="all-p">Переходов по ссылке</p>
+                  <p className="all-p">0</p>
+                </div>
+                <div className="changeover">
+                  <p className="all-p">Ваша скидка</p>
+                  <p className="all-p">0</p>
+                </div>
               </div>
-              <div className="changeover">
-                <p className="all-p">Ваша скидка</p>
-                <p className="all-p">0</p>
-              </div>
-              
-            </div></div>
-   
+            </div>
           </div>
         </div>
       </div>
