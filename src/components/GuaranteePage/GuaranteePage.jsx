010f0cd7546737bb878819b546764764ece8ac23
Timchinnn <95688018+Timchinnn@users.noreply.github.com>
2024-10-31 16:51:18 +0000

    modified:   src/components/GuaranteePage/GuaranteePage.jsx

--- src/components/GuaranteePage/GuaranteePage.jsx
+++ src/components/GuaranteePage/GuaranteePage.jsx
@@ -61,109 +61,112 @@ const GuaranteePage = ({ onClose }) => {
       <div className="images-container">
         <img src={bigB} alt="bigB" className="top-left" />
         <img src={bigBContray} alt="bigBContray" className="bottom-right" />
-        <img
-          className="arrow1"
-          src={arrow}
-          alt="arrow"
-          onClick={() => navigate(-1)}
-        />
-        <div className="parent1">
-          {/* <p>cd{ref}</p> */}
-          {/* <div className="window-block-about"></div> */}
-          <div className="share">
-            <p className="share-header-p">{t("Summoning friends")} </p>
-            <div className="earn">
-              <img src={arrowLong} alt="arrowLong" />
-              <p className="share-header-p">{t("make money")}</p>
-            </div>
-            <hr className="hr1" />
-            <p className="all-p1">
-              {t(
-                "According to your suggestion, each order can enjoy a 3% discount!"
-              )}
-            </p>
-            <p className="all-p">{t("How it works:")}</p>
-            <div className="about-sales">
-              <div className="about-sale">
-                <p>
-                  {t(
-                    "Get a unique link - click the button below and copy your personal link."
-                  )}
-                </p>
-              </div>
-              <div className="about-sale">
-                <p>
-                  {t(
-                    "Share with friends - send links to colleagues, partners, and acquaintances on Messenger or social media."
-                  )}
-                </p>
-              </div>
-              <div className="about-sale">
-                <p>
-                  {t(
-                    "Earn money with us - When you place an order through your link, you will receive 3% of the first check."
-                  )}
-                </p>
-              </div>
-            </div>
-            <hr className="hr2" />
-            <p className="all-p">
-              {t(
-                "Every month, the most active participants will receive valuable prizes:"
-              )}
-            </p>
-            <div className="arrow-groups">
-              <div className="arrow-group">
-                <img src={arrowDownL} alt="arrowDownL" />
-                <p>{t("First place: 7% discount")}</p>
-              </div>
-              <div className="arrow-group">
-                <img src={arrowDownM} alt="arrowDownM" />
-                <p>{t("Second place: 5% discount")}</p>
-              </div>
-              <div className="arrow-group">
-                <img src={arrowDownS} alt="arrowDownS" />
-                <p>{t("Third place: 3% discount")}</p>
-              </div>
-            </div>
-            <div className="statistic-hrad">
-              <div className="statistic">
-                <p className="all-p4">
-                  {t(
-                    "Real time statistics - tracking traffic and deposit the earned money into a personal office."
-                  )}
-                </p>
-              </div>
-            </div>
 
-            <p className="all-p2">
-              {t(
-                "Click the button below to get your unique link and start earning from today."
-              )}
-            </p>
-
-            <div className="arrowxs">
-              <img src={arrowXs} alt="arrowXs" />
-            </div>
-            <p className="all-p2">
-               {t("[Get the link and start earning money]")}
-            </p>
-            <div className="innovation-head">
-              {" "}
-              <div className="invitation">
-                <p className="all-p">{t("Your invitation link")}</p>
-                <p className="all-p" ref={linkRef}>
-                  {ref}
-                </p>
-                {/* <p className="all-p">https://t.me......................</p> */}
-                <button onClick={copyLink}>{t("Copy link")}</button>
-                <div className="changeover">
-                  <p className="all-p">{t("Click on the link")}</p>
-                  <p className="all-p">0</p>
+        <div>
+          <img
+            className="arrow1"
+            src={arrow}
+            alt="arrow"
+            onClick={() => navigate(-1)}
+          />{" "}
+          <div className="parent1">
+            {/* <p>cd{ref}</p> */}
+            {/* <div className="window-block-about"></div> */}
+            <div className="share">
+              <p className="share-header-p">{t("Summoning friends")} </p>
+              <div className="earn">
+                <img src={arrowLong} alt="arrowLong" />
+                <p className="share-header-p">{t("make money")}</p>
+              </div>
+              <hr className="hr1" />
+              <p className="all-p1">
+                {t(
+                  "According to your suggestion, each order can enjoy a 3% discount!"
+                )}
+              </p>
+              <p className="all-p">{t("How it works:")}</p>
+              <div className="about-sales">
+                <div className="about-sale">
+                  <p>
+                    {t(
+                      "Get a unique link - click the button below and copy your personal link."
+                    )}
+                  </p>
                 </div>
-                <div className="changeover">
-                  <p className="all-p">{t("Earned money")}</p>
-                  <p className="all-p">0</p>
+                <div className="about-sale">
+                  <p>
+                    {t(
+                      "Share with friends - send links to colleagues, partners, and acquaintances on Messenger or social media."
+                    )}
+                  </p>
+                </div>
+                <div className="about-sale">
+                  <p>
+                    {t(
+                      "Earn money with us - When you place an order through your link, you will receive 3% of the first check."
+                    )}
+                  </p>
+                </div>
+              </div>
+              <hr className="hr2" />
+              <p className="all-p">
+                {t(
+                  "Every month, the most active participants will receive valuable prizes:"
+                )}
+              </p>
+              <div className="arrow-groups">
+                <div className="arrow-group">
+                  <img src={arrowDownL} alt="arrowDownL" />
+                  <p>{t("First place: 7% discount")}</p>
+                </div>
+                <div className="arrow-group">
+                  <img src={arrowDownM} alt="arrowDownM" />
+                  <p>{t("Second place: 5% discount")}</p>
+                </div>
+                <div className="arrow-group">
+                  <img src={arrowDownS} alt="arrowDownS" />
+                  <p>{t("Third place: 3% discount")}</p>
+                </div>
+              </div>
+              <div className="statistic-hrad">
+                <div className="statistic">
+                  <p className="all-p4">
+                    {t(
+                      "Real time statistics - tracking traffic and deposit the earned money into a personal office."
+                    )}
+                  </p>
+                </div>
+              </div>
+
+              <p className="all-p2">
+                {t(
+                  "Click the button below to get your unique link and start earning from today."
+                )}
+              </p>
+
+              <div className="arrowxs">
+                <img src={arrowXs} alt="arrowXs" />
+              </div>
+              <p className="all-p2">
+                 {t("[Get the link and start earning money]")}
+              </p>
+              <div className="innovation-head">
+                {" "}
+                <div className="invitation">
+                  <p className="all-p">{t("Your invitation link")}</p>
+                  <p className="all-p" ref={linkRef}>
+                    {ref}
+                  </p>
+                  {/* <p className="all-p">https://t.me......................</p> */}
+                  <button onClick={copyLink}>{t("Copy link")}</button>
+                  <div className="changeover">
+                    <p className="all-p">{t("Click on the link")}</p>
+                    <p className="all-p">0</p>
+                  </div>
+                  <div className="changeover">
+                    <p className="all-p">{t("Earned money")}</p>
+                    <p className="all-p">0</p>
+                  </div>
                 </div>
               </div>
             </div>
